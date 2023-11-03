d2a173f4918b8d98bc5966178830f089a27ef45f
Malcolm Melbourne <malcolm_melbourne@outlook.com>
2023-11-03 19:26:22 -0400

    test

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -9,6 +9,22 @@ tailwind.config = {
 };
 
 
-const myName = "Malcolm";
-const nameplaceholder = document.getElementById("firstName");
-nameplaceholder.innerHTML = myName;
+// Assuming your JSON file is named data.json and is placed in the public directory
+const jsonUrl = 'data/basic.json';
+
+// Fetch the JSON file using the Fetch API
+fetch(jsonUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    // You can now use the 'data' object which contains the JSON data
+    const nameplaceholder = document.getElementById("firstName");
+    nameplaceholder.innerHTML = data.name;
+    console.log(data); // Example: log the data to the console
+  })
+  .catch((error) => {
+    console.error('Error fetching JSON:', error);
+  });
+
+  document.getElementById
+
+
